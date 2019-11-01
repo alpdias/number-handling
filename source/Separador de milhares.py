57d14503b85d65521faee2e749ce1cee8e2359a5
Paulo <al.pdias24@gmail.com>
2019-10-31 22:34:04 -0300

    Atualização do código

--- source/Separador de milhares.py
+++ source/Separador de milhares.py
@@ -1,4 +1,14 @@
-import locale # Biblioteca de localidade de padrões.
-locale.setlocale(locale.LC_ALL, "pt-BR") # Defini o padrão a ser utilizado.
-numero = 89000000 # Variável com um número.
-print(locale.format_string("%.2f", numero, grouping=True, monetary=True)) # Mostra a variável formatada no padrão escolhido.
+def Milhares(numero=0):
+    """
+    -> Função tratar o número separando por milhares no padrão do local.
+    :param numero: Número para ser formatado.
+    :return: Número formatado.
+    """
+    import locale # Biblioteca de localidade de padrões.
+    locale.setlocale(locale.LC_ALL, "pt-BR") # Defini o padrão a ser utilizado.
+    return (locale.format_string("%.2f", numero, grouping=True, monetary=True)) # Mostra a variável formatada no padrão escolhido.
+
+x = 1000
+y = 2000.56
+z = x + y
+print(f'{Milhares(z)}')
