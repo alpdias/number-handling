9d5cd6739080038106f8cef2a89280414e5c9ca5
Paulo <al.pdias24@gmail.com>
2020-01-01 13:22:27 -0300

    update codes and folders

--- src/tratamento-padrao.py
+++ src/tratamento-padrao.py
@@ -1,8 +1,9 @@
 '''
 @Autor: Paulo https://github.com/alpdias
 '''
-# Função para separa milhares de acordo com padrão do local.
-def milhares(numero=0):
+
+# Função para tratar números de acordo com padrão do local.
+def tratamento(numero=0):
     """
     -> Função para tratar o número separando por milhares no padrão do local.
     :param numero: Número para ser formatado.
@@ -12,7 +13,10 @@ def milhares(numero=0):
     locale.setlocale(locale.LC_ALL, "pt-BR") # Defini o padrão a ser utilizado.
     return (locale.format_string("%.2f", numero, grouping=True, monetary=True)) # Mostra a variável formatada no padrão escolhido.
 
+
+print('')
 x = 1000
 y = 2000.56
 z = x + y
-print(f'{milhares(z)}')
+print(f'{tratamento(z)}') # Resultado formatado no padrão 'pt-BR'.
+print('')
