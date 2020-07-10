882a6b66b1115cb46bf64aeae4c58074595e5ddb
Paulo Alcântara <52184239+alpdias@users.noreply.github.com>
2020-07-10 13:58:00 -0300

    Update tratamento.py

--- src/tratamento.py
+++ src/tratamento.py
@@ -2,21 +2,27 @@
 @Autor: Paulo https://github.com/alpdias
 '''
 
-# Função para tratar números de acordo com padrão do local.
+# Utilizando o método 'locale' para tratar números de acordo com padrão do local
 def tratamento(numero=0):
     """
-    -> Função para tratar o número separando por milhares no padrão do local.
-    :param numero: Número para ser formatado.
-    :return: Número formatado.
+    -> Função para tratar o número separando por milhares no padrão do local
+    :param numero: Número para ser formatado
+    :return: Número formatado
     """
-    import locale # Biblioteca de localidade de padrões.
-    locale.setlocale(locale.LC_ALL, "pt-BR") # Defini o padrão a ser utilizado.
-    return (locale.format_string("%.2f", numero, grouping=True, monetary=True)) # Mostra a variável formatada no padrão escolhido.
+    import locale # Biblioteca de localidade por padrão
+    locale.setlocale(locale.LC_MONETARY, "pt-BR") # Definições
+    return locale.currency(numero, grouping=True) # Mostra a variável formatada no padrão escolhido
+    '''
+    Parâmetros do metodo:
+    locale.LC_MONETARY -> Recebe a catergoria a ser utilizada (no caso valores monetários)
+    "pt-BR" -> Recebe o formato do local a ser utilizado (em sistemas UNIX pode ser achado o locale com o comando "locale -a")
+    grouping=True -> Agrupar o valor por casas decimais
+    '''
 
-
+    
 print('')
 x = 1000
 y = 2000.56
 z = x + y
-print(f'{tratamento(z)}') # Resultado formatado no padrão 'pt-BR'.
+print(f'{tratamento(z)}') # Resultado formatado no padrão 'pt-BR'
 print('')
